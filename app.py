f0ebf8de0bedd2bd31f2646cbb63362b0ad57c5e
TalalZahid014 <talalzahid012@gmail.com>
2024-05-22 09:45:04 +0500

    c

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask,request,jsonify
+import math
+app = Flask(__name__)
+@app.route('/hello/<var>', methods = ['GET'] )
+def hello(var):
+  return f"Hi,{var}"
+@app.route('/square/<int:number>')
+def square_number(number):
+    return jsonify({
+        "number": number,
+        "square": number ** 2
+    }) 
+@app.route('/quadratic/<float:a>/<float:b>/<float:c>', methods = ['GET'])
+def quadratic(a,b,c):
+   discriminant = b**2 - 4*a*c
+   if discriminant < 0:
+      return jsonify({"error": "No real roots exist as the discriminant is negative."}), 400
+   root_1 = (-b + math.sqrt(discriminant))/(2*a)
+   print('first root',root_1)
+   root_2 = (-b - math.sqrt(discriminant))/(2*a)
+   print('second root',root_2)
+   return jsonify({
+    "first root": root_1,
+    "second root": root_2
+  })
+@app.route('/pressure/<float:area>/<float:force>', methods = ['GET'])
+def pressure(area, force):
+  pressure_value = force / area
+  if pressure_value < 0:
+    return jsonify({"error":"pressure is less than zero"}), 400
+  else:
+    return jsonify({
+      "pressure": pressure_value
+    })
+@app.route('/orbital-speed/<radius>/<time>')
+def orbital(radius,time):
+   orbital_speed = (radius * 2 * math.pi) / time
+   return jsonify({
+      "orbital-speed": orbital_speed
+   }) 
+@app.route('/pythagoras/<base>/<height>')
+def pythagoras(base,height):
+   hyp = math.sqrt((base ** 2) + (height ** 2))
+   return jsonify({
+      "hypotenuse": hyp
+   })
+# @app.route('/')
+if __name__ == '__main__':
+  app.run(debug = True)
