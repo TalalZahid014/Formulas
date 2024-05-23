d403d2418aacfd525aead50904e6c1f389b3bb75
TalalZahid014 <talalzahid012@gmail.com>
2024-05-23 20:17:15 +0500

    f

--- app.py
+++ app.py
@@ -32,89 +32,90 @@ def pressure(area, force):
     return jsonify({
       "pressure": pressure_value
     })
-@app.route('/orbital-speed/<radius>/<time>')
+@app.route('/orbital-speed/<int:radius>/<int:time>')
 def orbital(radius,time):
    orbital_speed = (radius * 2 * math.pi) / time
    return jsonify({
       "orbital-speed": orbital_speed
    }) 
-@app.route('/pythagoras/<base>/<height>')
+@app.route('/pythagoras/<int:base>/<int:height>')
 def pythagoras(base,height):
    hyp = math.sqrt((base ** 2) + (height ** 2))
    return jsonify({
       "hypotenuse": hyp
    })
-@app.route('/cube/<number>')
+@app.route('/cube/<int:number>')
 def cube(number):
    cube_value = number ** 3
    return jsonify({
       "cube": cube_value
    })
-@app.route('/square-root/<number>')
+@app.route('/square-root/<int:number>')
 def sqrt(number):
    square_root = math.sqrt(number)
    return jsonify({
       "square root": square_root
    })
-@app.route('/cube-root/<number>')
+@app.route('/cube-root/<int:number>')
 def cube_rt(number):
    crt = math.cbrt(number)
    return jsonify({
       "cube root": crt
    })
-@app.route('/power-x/<number>/<x>')
+@app.route('/power-x/<int:number>/<int:x>')
 def power(number,x):
    pwer = number ** x
    return jsonify({
       "answer": pwer
    })
-@app.route('/divide/<x>/<y>')
+@app.route('/divide/<int:x>/<int:y>')
 def divide(x,y):
    div = x / y
    return jsonify({
       "answer": div
    })
-@app.route('/multiply/<x>/<y>')
+@app.route('/multiply/<int:x>/<int:y>')
 def multiply(x,y):
    multiple = x * y
    return jsonify({
       "answer": multiple
    })
-@app.route('/kinetic-energy/<mass>/velocity')
+@app.route('/kinetic-energy/<int:mass>/<int:velocity>')
 def E_K(mass,velocity):
    energy = ((velocity ** 2) * mass) / 2
    return jsonify({
      "Kinetic Energy":energy
    })
-@app.route('/gravitiational-potential-energy/<mass>/<height>')
+@app.route('/gravitiational-potential-energy/<int:mass>/<int:height>')
 def G_P_E(mass,height):
    gpe = mass * height * 9.81
    return jsonify({
       "Gravitational Potential Energy": gpe
    })
-@app.route('/acceleration/<velocity>/<time>')
+@app.route('/acceleration/<int:velocity>/<int:time>')
 def accelaration(velocity,time):
    acc = velocity / time
    return jsonify({
       "acceleration":acc
    })
-@app.route('/area-trapezium/<a>/<b>/<h>')
+@app.route('/area-trapezium/<int:a>/<int:b>/<int:h>')
 def trapezium(a,b,h):
    trap = ((a + b) * h)
    return jsonify({
       "area": trap
    })
-@app.route('/volume-cone/<radius>/<height>')
+@app.route('/volume-cone/<int:radius>/<int:height>')
 def cone(radius,height):
    volume = (((radius ** 2) * height) * math.pi) / 3
    return jsonify({
       "volume": volume
    })
-@app.route('/volume-sphere/<radius>')
+@app.route('/volume-sphere/<int:radius>')
 def sphere(radius):
    volume = (4 * math.pi * (radius ** 3)) / 3
    return jsonify({
       "volume": volume
    })
+# @app.route('/')
 if __name__ == '__main__':
   app.run(debug = True)
